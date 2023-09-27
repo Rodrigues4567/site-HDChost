f2de0972cfd191c17153459782b767b2ee1aed15
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2023-09-27 18:57:42 -0300

    Versão mobile first PRONTA!

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ button.addEventListener('click', menuShow = () => {
 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = 'icones/menu.svg'
     }
     else {
         menuMobile.classList.add('open')
+        document.querySelector('.icon').src = 'icones/alfabeto.png'
     }
 })
